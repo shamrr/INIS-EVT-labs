e6e6e35a54aa96b717c7ffc034c43de067fa0633
Anastasya <shamr44@mail.ru>
2023-02-06 12:47:08 +0300

    edit main.js file

--- inis/inis_1/main.js
+++ inis/inis_1/main.js
@@ -4,21 +4,20 @@ let numberOfFilms = prompt('Сколько фильмов вы уже посмо
 //2
 const personalMovieDB = {
     count: numberOfFilms,
-    movies: {},
+    movies: {}
 }
 
 //3
 for(let i = 0; i < 2; i++) {
-    let a = prompt('Один из последних просмотренных фильмов?'),
-        b = prompt('На сколько оцените его?');
-}
+    let a = prompt('Один из последних просмотренных фильмов?');
+    let b = prompt('На сколько оцените его?');
 
-//4,5
-if(a != '' && b != '' && a != null && b != null  && a.length < 50) {
-    console.log('Введите значение');
-} else {
-    personalMovieDB.movies[a] = b;
+    //4,5
+    if(a != '' && b != '' && a != null && b != null  && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+    } else {
+        console.log('Введите значение');
+    }
 }
-
 //6
 console.log(personalMovieDB);
