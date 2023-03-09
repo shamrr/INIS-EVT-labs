4d3d53355e86933c21a23fe1c6b1ccd9785b45a9
Anastasya <shamr44@mail.ru>
2023-03-09 17:41:10 +0300

    added evt_4

---
+++ inis/inis_3/shirts.js
@@ -0,0 +1,124 @@
+const shirts = [{
+    "name": "Beep Boop",
+    "description": "Once upon a time, a mighty guide guarded the intersection of Forbes and Morewood, and would dutifully direct distracted college students when it was safe to cross the street. Its voice was soothing, strong, and steady. Its name was beep boop.",
+    "price": "$19.99",
+    "colors": {
+        "white": {
+            "front": "shirt_images/beepboop-white-front.png",
+            "back": "shirt_images/beepboop-white-back.png"
+        },
+        "blue": {
+            "front": "shirt_images/beepboop-blue-front.png",
+            "back": "shirt_images/beepboop-blue-back.png"
+        },
+        "pink": {
+            "front": "shirt_images/beepboop-pink-front.png",
+            "back": "shirt_images/beepboop-pink-back.png"
+        },
+        "red": {
+            "front": "shirt_images/beepboop-red-front.png",
+            "back": "shirt_images/beepboop-red-back.png"
+        }
+    },
+    "default": {
+        "front": "shirt_images/default-m-front.png",
+        "back": "shirt_images/default-m-back.png"
+    }
+},
+{
+    "name": "Car",
+    "description": "As you move in to campus, one of the first memories so many students have is driving up to their dorm, unloading their bags, and moving in. How do they arrive to campus? By car, of course!",
+    "price": "$10.99",
+    "colors": {
+        "white": {
+            "front": "shirt_images/car-white-front.png",
+            "back": "shirt_images/car-white-back.png"
+        },
+        "blue": {
+            "front": "shirt_images/car-blue-front.png",
+            "back": "shirt_images/car-blue-back.png"
+        },
+        "green": {
+            "front": "shirt_images/car-green-front.png",
+            "back": "shirt_images/car-green-back.png"
+        },
+        "yellow": {
+            "front": "shirt_images/car-yellow-front.png",
+            "back": "shirt_images/car-yellow-back.png"
+        },
+        "red": {
+            "front": "shirt_images/car-red-front.png",
+            "back": "shirt_images/car-red-back.png"
+        }
+    },
+    "default": {
+        "front": "shirt_images/default-w-front.png",
+        "back": "shirt_images/default-w-back.png"
+    }
+},
+{
+    "name": "Forever Plaid",
+    "price": "$13.99",
+    "description": "Proudly wear your tartan plaid as a patch on your front shirt pocket. And on the back, ask the important question that all CMU students should know the answer to: got plaid?",
+    "colors": {
+        "white": {
+            "front": "shirt_images/plaid-white-front.png",
+            "back": "shirt_images/plaid-white-back.png"
+        },
+        "pink": {
+            "front": "shirt_images/plaid-pink-front.png",
+            "back": "shirt_images/plaid-pink-back.png"
+        }
+    },
+    "default": {
+        "front": "shirt_images/default-w-front.png",
+        "back": "shirt_images/default-w-back.png"
+    }
+},
+{
+    "name": "BSUIR",
+    "description": "BSUIR mission is to train engineers and scientists capable of generating and implementing innovative ideas, creating competitive high technology products in the spheres of computer science and electronics.",
+    "price": "$6.99",
+    "colors": {
+        "white": {
+            "front": "shirt_images/bsuir-white-front.png",
+            "back": "shirt_images/bsuir-white-back.png"
+        }
+    },
+    "default": {
+        "front": "shirt_images/default-m-front.png",
+        "back": "shirt_images/default-m-back.png"
+    }
+}];
+
+const body = document.querySelector('body');
+function createCard (shirts){
+    const card = document.createElement('div');
+    card.className = 'card'
+
+    const img = document.createElement('img');
+    img.className = 'card__img'
+
+    const title = document.createElement('h1');
+    name.className = 'card__title';
+
+    const subtitle = document.createElement('div');
+    subtitle.className = 'card__subtitle';
+
+    const btnContainer = document.createElement('div');
+    btnContainer.className = 'card__btn-container';
+
+    const btnQuickView = document.createElement('button');
+    btnQuickView.className = 'btn btn__view';
+    btnQuickView.innerText = 'Oquick View';
+
+    const btnSeePage = document.createElement('button');
+    btnSeePage.className = 'btn btn__see';
+    btnSeePage.innerText = 'See Page';
+
+    btnContainer.append(btnQuickView, btnSeePage);
+    card.append(img, title, subtitle, btnContainer);
+    body.append(card);
+    return card;
+}
+createCard();
