4eff4d6cb44f8b9159688b0e8588c9adeab8efa1
Anastasya <shamr44@mail.ru>
2023-03-30 11:39:33 +0300

    create inis_4

---
+++ inis/inis_3/detail.js
@@ -0,0 +1,48 @@
+import { shirts } from "./shirts.js";
+
+let main_inner = document.querySelector(".main_inner");
+
+function detailView() {
+  let index = localStorage.getItem("index");
+
+  let block = document.createElement("div")
+  block.classList.add("block")
+
+  let img_container = document.createElement("div")
+  img_container.classList.add("img_container")
+  
+  let detail_img = document.createElement("img")
+  detail_img.classList.add("detail_img")
+  detail_img.src = shirts[index].colors.white.front
+
+  let info_block = document.createElement("div")
+  info_block.classList.add("info_block")
+
+  let detail_name = document.createElement("h3")
+  detail_name.classList.add("detail_name")
+  detail_name.innerText = shirts[index].name
+
+  let detail_price = document.createElement("p")
+  detail_price.classList.add("detail_price")
+  detail_price.innerText = shirts[index].price
+
+  let back_link = document.createElement("a")
+  back_link.classList.add("back_link")
+  back_link.innerText = "Back"
+  back_link.setAttribute("href", "https://main--mellow-arithmetic-c1683f.netlify.app/inis_lab_3/inis_lab_3.html")
+
+  let detail_description = document.createElement("p")
+  detail_description.classList.add("detail_description")
+  detail_description.innerText = shirts[index].description
+  
+  info_block.appendChild(detail_name)
+  info_block.appendChild(detail_price)
+  info_block.appendChild(detail_description)
+  info_block.appendChild(back_link)
+  img_container.appendChild(detail_img)
+  block.appendChild(img_container)
+  block.appendChild(info_block)
+  main_inner.appendChild(block)
+};
+
+detailView()
---
+++ inis/inis_3/list.js
@@ -0,0 +1,66 @@
+import { shirts } from './shirts.js'
+
+
+let main_inner = document.querySelector(".main_inner")
+
+let content = document.createElement("div");
+content.classList.add("content")
+
+function build_goods_list(){
+
+    shirts.forEach((shirt, iterator) => {
+        let card_container = document.createElement("div");
+        card_container.classList.add('card_container');
+
+        let card_image = document.createElement("img");
+        card_image.classList.add('card_image');
+        card_image.src = shirt.colors.white?.front
+
+        let card_counter = document.createElement("p");
+        card_counter.classList.add('card_header')
+        card_counter.innerText = iterator + 1
+
+        let card_header = document.createElement("h3");
+        card_header.classList.add('card_header');
+        card_header.innerText = shirt.name
+
+        let card_available_count = document.createElement("p")
+        card_available_count.classList.add('card_available_count');
+        card_available_count.innerText = string_available(Object.keys(shirt.colors).length)
+
+        let quick_view_button = document.createElement("button");
+        quick_view_button.classList.add('quick_view_button');
+        quick_view_button.innerText = 'Quick View'
+        quick_view_button.setAttribute("type","submit")
+
+        let detail_view_button = document.createElement("button");
+        detail_view_button.classList.add('quick_view_button')
+        detail_view_button.innerText = 'Detail View'
+        detail_view_button.setAttribute("type", "submit")
+        detail_view_button.setAttribute("id", iterator)
+        
+        card_container.appendChild(card_image)
+        card_container.appendChild(card_counter)
+        card_container.appendChild(card_header)
+        card_container.appendChild(card_available_count)
+        card_container.appendChild(quick_view_button)
+        card_container.appendChild(detail_view_button)
+        content.appendChild(card_container)
+    });
+    main_inner.appendChild(content)
+
+}
+
+document.addEventListener('click',e => {
+    let id = e.target.getAttribute('id')
+    localStorage.setItem('index', id)
+    window.open('http://127.0.0.1:5500/inis/inis_3/detail.html')
+})
+
+
+function string_available(quantity) {
+    return `Available in ${quantity} colors`
+}
+
+
+build_goods_list();
