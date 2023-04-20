7178c256641bde796e7d920c8e76b1475a1fe165
Nastya <shamr44@mail.ru>
2023-04-20 12:37:22 +0300

    inis_lab5 done

--- inis/inis_5/input.js
+++ inis/inis_5/input.js
@@ -7,7 +7,7 @@ elements.forEach(element => {
             element.style.left = pageX - element.offsetWidth / 2 + 'px'
             element.style.top = pageY - element.offsetHeight /2 + 'px'
         }
-
+        
         function onMouseMove(event){
             moveAt(event.pageX, event.pageY);
         }
@@ -17,5 +17,32 @@ elements.forEach(element => {
         document.addEventListener('click', function(){
             document.removeEventListener('mousemove', onMouseMove)
         })
+
+        document.addEventListener('keydown', function handleEscDown(event){
+            if (event.code == 'Escape'){
+                document.removeEventListener('mousemove', onMouseMove)
+            }
+        })
+    })
+
+    element.addEventListener('dblclick', function handleDbClick(event){
+
+        function moveAt(pageX, pageY){
+            element.style.left = pageX - element.offsetWidth / 2 + 'px'
+            element.style.top = pageY - element.offsetHeight /2 + 'px'
+        }
+        
+        function onMouseMove(event){
+            moveAt(event.pageX, event.pageY);
+        }
+        
+        document.addEventListener('mousemove', onMouseMove)
+
+        document.addEventListener('keydown', function handleEscDown(event){
+            if (event.code == 'Escape'){
+                document.removeEventListener('mousemove', onMouseMove)
+            }
+        })
     })
 })
+
